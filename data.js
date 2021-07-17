9a7eb5c728f529e6cd165d94b9419cdf2b9db7a5
KyleLynnRas <kylelynn.ras@gmail.com>
2021-07-17 09:46:19 -0400

    Update for netlify

--- data.js
+++ data.js
@@ -1,19 +1,21 @@
 export const data = [
-    {
-        title: "TroupeLoop", 
-        summary: "A message board for performance troupes to help keep everyone in the loop on performances, practices and everything else. Users are able to create an account and log into the shared team’s message board. There they can create posts and comments. Authorization is used to ensure users can only view pages if logged in and only edit/delete comments that they created. Users also have the ability to favorite posts and comments, and view them on a personalized favorites page.",
-        tech: "HTML5, CSS3, JavsScript, jQuery, Express, MongoDB, Mongoose, Node.js, Bootstrap",
-        video: "https://i.imgur.com/fOqrwyk.mp4",
-        liveUrl: "https://unit2projectkr.herokuapp.com/",
-        frontendUrl: "https://github.com/KyleLynnRas/TroupeLoop"
-    }, 
-    {
-        title: "npm start career", 
-        summary: "An app to help software engineering bootcamp students stay organized in their dev job search. Users can search for jobs from the Remotive API, save jobs and add/update notes and a checklist to saved jobs throughout the application cycle.",
-        tech: "HTML5, CSS3, JavsScript, React, Express, MongoDB, Mongoose, Sass, JWT, Node.js, Bulma",
-        video: "https://i.imgur.com/LhbvGcM.mp4",
-        liveUrl: "https://npmstartcareer.netlify.app/",
-        frontendUrl: "https://github.com/KyleLynnRas/proj3-frontend",
-        backendUrl: "https://github.com/KyleLynnRas/proj3-backend"
-    }
-  ]
+	{
+		title: "TroupeLoop",
+		summary:
+			"A message board for performance troupes to help keep everyone in the loop on performances, practices and everything else. Users are able to create an account and log into the shared team’s message board. There they can create posts and comments. Authorization is used to ensure users can only view pages if logged in and only edit/delete comments that they created. Users also have the ability to favorite posts and comments, and view them on a personalized favorites page.",
+		tech: "HTML5, CSS3, JavsScript, jQuery, Express, MongoDB, Mongoose, Node.js, Bootstrap",
+		video: "https://i.imgur.com/fOqrwyk.mp4",
+		liveUrl: "https://unit2projectkr.herokuapp.com/",
+		frontendUrl: "https://github.com/KyleLynnRas/TroupeLoop",
+	},
+	{
+		title: "npm start career",
+		summary:
+			"An app to help software engineering bootcamp students stay organized in their dev job search. Users can search for jobs from the Remotive API, save jobs and add/update notes and a checklist to saved jobs throughout the application cycle.",
+		tech: "HTML5, CSS3, JavsScript, React, Express, MongoDB, Mongoose, Sass, JWT, Node.js, Bulma",
+		video: "https://i.imgur.com/LhbvGcM.mp4",
+		liveUrl: "https://npmstartcareer.netlify.app/",
+		frontendUrl: "https://github.com/KyleLynnRas/proj3-frontend",
+		backendUrl: "https://github.com/KyleLynnRas/proj3-backend",
+	},
+];
